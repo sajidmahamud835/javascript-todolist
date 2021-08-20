8775638d33a20609c8743871536d3f296a9f96b1
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-20 19:26:15 +0600

    javascript linked with css, event listener added

---
+++ js/todolist.js
@@ -0,0 +1,13 @@
+//Global Variables
+const addTaskButton = document.getElementById('add-task-button');
+const addTaskInput = document.getElementById('add-task-input');
+
+/* -------------------
+    Add Task Function
+---------------------- */
+
+//Event listener
+
+addTaskButton.addEventListener('click', function () {
+    console.log('add task button clicked')
+});

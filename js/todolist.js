8870fa5991242f3883d45a753825f8c5ddfba5ca
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-20 20:32:37 +0600

    add task function added

--- js/todolist.js
+++ js/todolist.js
@@ -1,13 +1,41 @@
 //Global Variables
 const addTaskButton = document.getElementById('add-task-button');
 const addTaskInput = document.getElementById('add-task-input');
+const taskList = document.getElementById('task-list');
 
 /* -------------------
     Add Task Function
 ---------------------- */
 
+//Event handeler
+function addTaskButtonAction() {
+    console.log('add task button action started')
+
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const span = document.createElement('span');
+
+    span.innerText = addTaskInput.value;
+
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", "checkbox");
+    checkbox.classList.add('me-2')
+
+    li.classList.add('list-group-item');
+    li.appendChild(checkbox);
+    li.appendChild(span)
+
+    ul.classList.add('list-group')
+    ul.appendChild(li)
+
+    taskList.appendChild(ul)
+
+}
+
 //Event listener
 
 addTaskButton.addEventListener('click', function () {
     console.log('add task button clicked')
+    addTaskButtonAction();
 });

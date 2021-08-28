081c5e07375798a756a7fd556662f08db9e8887a
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-29 00:40:54 +0600

    creating checkbox using icon instand of input

--- js/todolist.js
+++ js/todolist.js
@@ -1,7 +1,15 @@
-//Global Variables
+/*------------------------
+     Global Variables
+------------------------- */
+
+//selcetor variables
 const addTaskButton = document.getElementById('add-task-button');
 const addTaskInput = document.getElementById('add-task-input');
 const taskList = document.getElementById('task-list');
+const inputCheckboxs = document.getElementsByClassName('far');
+
+//statics variables
+
 
 /* -------------------
     Add Task Function
@@ -11,19 +19,21 @@ const taskList = document.getElementById('task-list');
 function addTask() {
     console.log('add task button action started')
 
+    //creates ui, li, input and span tag
     const ul = document.createElement('ul');
     const li = document.createElement('li');
-    const checkbox = document.createElement('input');
+    const checkbox = document.createElement('i');
     const span = document.createElement('span');
 
-    span.innerText = addTaskInput.value;
+    span.innerText = addTaskInput.value; // puts the inputed value in the span tag.
 
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("id", "checkbox");
+    //set attribue for the input tag and make it a checkbox.
+    checkbox.setAttribute("class", "far fa-check-square");
     checkbox.classList.add('me-2')
 
-    li.classList.add('list-group-item');
-    li.appendChild(checkbox);
+
+    li.classList.add('list-group-item'); //adds a class name.
+    li.appendChild(checkbox); //appending its child elements.
     li.appendChild(span)
 
     ul.classList.add('list-group')
@@ -33,6 +43,9 @@ function addTask() {
 
     // clear input field
     addTaskInput.value = ''
+
+    //call event listener after adding each task
+    eventListener()
 
 }
 
@@ -50,3 +63,25 @@ addTaskInput.addEventListener('keyup', function (event) {
         addTask();
     }
 });
+
+
+/* -------------------
+    Done Task Funciton
+---------------------- */
+
+console.log(inputCheckboxs[0]);
+
+
+//Eevent listener
+function eventListener() {
+    for (inputCheckbox of inputCheckboxs) {
+        inputCheckbox.addEventListener('click', function (event) {
+
+            console.log('checkbox checked');
+            event.target.classList.add('fas')
+            event.target.parentNode.classList.add('disabled');
+
+        });
+    }
+}
+
